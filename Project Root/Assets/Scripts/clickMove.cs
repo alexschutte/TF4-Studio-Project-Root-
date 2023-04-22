c2ae294d29066089e2b9341db8ae53ad84b38d0f
Jaden Lawrence <jaden2.lawrence@live.uwe.ac.uk>
2023-04-23 00:24:56 +0100

    Quit and retry buttons
    Quit button returns to main menu. Retry button starts the level over

--- Project Root/Assets/Scripts/clickMove.cs
+++ Project Root/Assets/Scripts/clickMove.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class clickMove : MonoBehaviour
 {
     public float speed = 5f;
-    Vector2 movement;
     [SerializeField] private int tileNo;
-    Vector3 clickPos = new Vector3(-1.5f, 0.35f, -2f);
-    bool moving;
+    Vector3 clickPos = new Vector3(-1.5f, -0.28f, -1f);
     private Globals global;
 
     void OnMouseOver()
@@ -23,7 +21,6 @@ public class clickMove : MonoBehaviour
                 if (tileNo == global.playerCurrentPos - 1 || tileNo == global.playerCurrentPos + 1 || tileNo == global.playerCurrentPos - 4 || tileNo == global.playerCurrentPos + 4)
                 {
                     clickPos = transform.position;
-                    moving = true;
                     PlayerCharacter.transform.position = new Vector3(clickPos.x, clickPos.y, -2);
                     global.playerCurrentPos = tileNo;
                     global.AP--;
