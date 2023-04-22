ff93cf9fff96efacf2f52f501e5700d1d44147ae
Jaden Lawrence <jaden2.lawrence@live.uwe.ac.uk>
2023-04-22 23:06:09 +0100

    UI + Root + Move/Fire toggle
    Added UI to the gameplay, added the ability to root to regain AP, and added a toggle for moving or firing

--- Project Root/Assets/Scripts/Globals.cs
+++ Project Root/Assets/Scripts/Globals.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Globals : MonoBehaviour
 {
     public int playerCurrentPos;
     public int AP;
+    public int turn;
+    [SerializeField] public Text APCounter;
+    [SerializeField] public Text turnCounter;
+    public bool moving;
 
     private void Awake()
     {
         playerCurrentPos = 17;
         AP = 5;
+        turn = 1;
+        APCounter.text = "AP: " + AP.ToString();
+        turnCounter.text = "Turn " + turn.ToString();
+        moving = true;
     }
 }
