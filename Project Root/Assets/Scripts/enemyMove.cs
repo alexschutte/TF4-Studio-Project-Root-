cb0c65e5b93683cd8df2a8d88985294ab2d63b50
Alex <alexander2.schutte@live.uwe.ac.uk>
2023-05-01 16:07:44 +0100

    random movement

--- Project Root/Assets/Scripts/enemyMove.cs
+++ Project Root/Assets/Scripts/enemyMove.cs
@@ -4,15 +4,87 @@ using UnityEngine;
 
 public class enemyMove : MonoBehaviour
 {
+
     private Globals global;
-    [SerializeField]private GameObject enemy;
+    private GameObject enemyCharacter; 
+
+    private void Awake()
+    {
+        enemyCharacter = GameObject.FindGameObjectWithTag("enemie");
+        global = GameObject.FindGameObjectWithTag("GameController").GetComponent<Globals>();
+        NewRandomNumber();
+    }
+
+
+    int ranNum;
+    void NewRandomNumber()
+    {
+        ranNum = Random.Range(1, 5);
+    }
+
 
     void Update()
     {
         if(global.enemyAP > 0)
         {
-            
+            if(ranNum == 1)
+            {
+                if(global.enemyCurrentPos < 5)
+                {
+                    NewRandomNumber();
+                }
+                else
+                {
+                    enemyCharacter.transform.position = new Vector3(enemyCharacter.transform.position.x, enemyCharacter.transform.position.y + 1, -2);
+                    global.enemyCurrentPos = global.enemyCurrentPos + 4;
+                    global.enemyAP--;
+                    NewRandomNumber();
+                }
+            }
+            if(ranNum == 2)
+            {
+                if(global.enemyCurrentPos == 4 || global.enemyCurrentPos == 8 || global.enemyCurrentPos == 12 || global.enemyCurrentPos == 16 || global.enemyCurrentPos == 20)
+                {
+                    NewRandomNumber();
+                }
+                else
+                {
+                    enemyCharacter.transform.position = new Vector3(enemyCharacter.transform.position.x + 1, enemyCharacter.transform.position.y, -2);
+                    global.enemyCurrentPos = global.enemyCurrentPos + 1;
+                    global.enemyAP--;
+                    NewRandomNumber();
+                }
+            }
+            if(ranNum == 3)
+            {
+                if(global.enemyCurrentPos > 16)
+                {
+                    NewRandomNumber();
+                }
+                else
+                {
+                    enemyCharacter.transform.position = new Vector3(enemyCharacter.transform.position.x, enemyCharacter.transform.position.y - 1, -2);
+                    global.enemyCurrentPos = global.enemyCurrentPos + 4;
+                    global.enemyAP--;
+                    NewRandomNumber();
+                }
+            }
+            if(ranNum == 4)
+            {
+                if(global.enemyCurrentPos == 1 || global.enemyCurrentPos == 5 || global.enemyCurrentPos == 9 || global.enemyCurrentPos == 13 || global.enemyCurrentPos == 17)
+                {
+                    NewRandomNumber();
+                }
+                else
+                {
+                    enemyCharacter.transform.position = new Vector3(enemyCharacter.transform.position.x - 1, enemyCharacter.transform.position.y, -2);
+                    global.enemyCurrentPos = global.enemyCurrentPos - 1;
+                    global.enemyAP--;
+                    NewRandomNumber();
+                }
+            }
         }
-
+        Debug.Log(global.enemyCurrentPos + " enPos");
+        Debug.Log(ranNum + " rannum");
     }
 }
